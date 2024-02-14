d89b0a707d5a8de764dc7ae955d9fde4ce98d521
aashikkk <sihaabama@gmail.com>
2024-02-14 17:20:24 +0530

    adding files

---
+++ lib/FirstScreen.dart
@@ -0,0 +1,71 @@
+import 'package:fitnessapp/SelectMethod.dart';
+import 'package:fitnessapp/UserRecordManager.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'SignUp.dart';
+import 'HomeScreen.dart';
+import 'set_height_and_width.dart';
+
+class firstscreen extends StatefulWidget {
+  const firstscreen({Key? key}) : super(key: key);
+
+  @override
+  State<firstscreen> createState() => _firstscreenState();
+}
+
+class _firstscreenState extends State<firstscreen> {
+  final _auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/mainscreenpic.jpg'),
+          fit: BoxFit.cover,
+        )),
+        child: Container(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: InkWell(
+                onTap: () {
+                  if (_auth.currentUser != null) {
+                    UserRecords().Useremail =
+                        _auth.currentUser?.email.toString();
+                    UserRecords().Userid = _auth.currentUser?.uid.toString();
+
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => homescreen()));
+                  } else {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => signup()));
+                  }
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: getheight(context) * 0.9),
+                  width: getwidth(context) * 0.85,
+                  height: getheight(context) * 0.065,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                  ),
+                  child: Center(
+                    child: Text('Get Started',
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 0.5,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 21.0,
+                        )),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
